e8c8d737b78b8464c66c3c26c87cd142b560b3fa
ahmed rashed <ahmedxyx93@gmail.com>
2025-12-11 08:56:25 +0300

    connnecting apii

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,2 @@
 export {default as ScreensContainer} from './ScreensContainer';
-
-export {default as GradientText} from './GradientText';
-export {default as AppHeader} from './AppHeader';
-export {default as HeadPage} from './HeadPage';
-export {default as LottieLoader} from './LottieLoader';
-export {default as EmptyState} from './EmptyState';
+export {default as CustomToast} from './CustomToast';
